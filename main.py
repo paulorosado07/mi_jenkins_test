64ede33d16ccda57f1f9c3315daf21e7ceb66baf
Paulo <94853750+paulorosado07@users.noreply.github.com>
2025-01-01 19:57:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('Hello My Name Is Paulo Rosado Hahahahahahahahahaha!')
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__, static_folder='static')
+app.config['JSON_AS_ASCII'] = False
+
+cors = CORS(app, resource={
+    r"/*":{
+        "origin":"*"
+    }
+})
+
+
+
+@app.route("/")
+def home():    
+    return ({"message": "Hello World!"})
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
